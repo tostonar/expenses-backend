110c741fb0aa1a4465f6e9c7de6ed5e64267ec90
Andrea T <tostonar@gmail.com>
2020-10-20 15:38:44 -0400

    set up both controllers and update_balance  account  instance method

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,45 @@
 class Api::V1::TransactionsController < ApplicationController
+
+  before_action :set_account
+
+  def index
+    @transactions = @account.transactions
+    render json: @transactions
+  end
+
+  def create
+    @transaction = @account.transactions.new(transaction_params)
+    if @account.update_balance(@transaction) != 'Balance too low.'
+      @transaction.save
+      render json: @transaction
+    else
+      render json: {error: 'Balance too low'}
+    end
+  end
+
+  def show
+    @transaction = Transaction.find(params[:id])
+    render json: @transaction
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_account
+    @account = Account.find(params[:account_id])
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:account_id, :amount, :kind, :date, :description)
+  end
+
+
+
+
+
+
+
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,19 @@
 class Account < ApplicationRecord
   has_many :transactions
   validates :name, :balance, presence: true
+
+  def update_balance(transaction)
+     if transaction.kind == 'deposit'
+       self.balance += transaction.amount
+       self.save
+     elsif transaction.kind == 'withdraw'
+      if self.balance >= transaction.amount
+        self.balance -= transaction.amount
+        self.save
+      else
+        return 'Balance too low.'
+      end
+     end
+  end
+
 end
